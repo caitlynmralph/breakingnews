4428b68d70026e8c5a37fc8a77157003b304ab8f
caitlynralph <caitlyn.ralph14@ncf.edu>
2017-08-21 17:52:22 -0400

    added new data

--- code/json_prettyify.py
+++ code/json_prettyify.py
@@ -3,22 +3,22 @@ import os
 import csv
 import numpy as np
 
-os.chdir("/Applications/MAMP/htdocs/breakingnews")
+os.chdir("/Applications/MAMP/htdocs/breakingnews/spreadsheets")
 
-with open("events_100days.json","r") as f:
+with open("charlottesville_alerts.json","r") as f:
     data_parsed = json.load(f)
 
-data_tsv = open("/Applications/MAMP/htdocs/breakingnews/events_100days.tsv","w")
+data_tsv = open("/Applications/MAMP/htdocs/breakingnews/spreadsheets/charlottesville_alerts.tsv","w")
 
 csvwriter = csv.writer(data_tsv, delimiter="|")
 
 count = 0
 
-for event in data_parsed:
+for alert in data_parsed:
     if count == 0:
-        header = event.keys()
+        header = alert.keys()
         csvwriter.writerow(header)
         count += 1
-    csvwriter.writerow(event.values())
+    csvwriter.writerow(alert.values())
 
 data_tsv.close()
